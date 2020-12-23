4d51dffbe28af1d5b9946ccdacc7ec30a18281c1
alfonso904 <74293312+alfonso904@users.noreply.github.com>
2020-12-23 06:03:24 -0900

    update

---
+++ ACWD.js
@@ -0,0 +1,18 @@
+$('.nav__trigger',).on('click', function(e){
+
+  // prevent default behavior
+  e.preventDefault(); 
+
+  // add nav--active class to nav icon
+  $(this).parent().toggleClass('nav--active').show(); 
+
+  // disable scroll behind nav
+  $('body').toggleClass('no-scroll'); 
+
+});
+
+$('.nav li, .nav').on('click', function(){
+  $(".nav__trigger").parent().removeClass("nav--active");
+  $(".nav__icon").removeClass("nav--active");
+  $('body').removeClass('no-scroll');
+});
